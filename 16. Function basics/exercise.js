efcf7bef753527b3fd6b401b93658833b528bb07
Franzl76 <60292525+Franzl76@users.noreply.github.com>
2021-12-15 13:03:23 +0000

    Add files via upload

---
+++ 16. Function basics/exercise.js
@@ -0,0 +1,22 @@
+//Functie definieren voor pizza maken
+const makePizza = function () {
+    console.log('Shape the deeg in circle');
+    console.log('Add Tomatosauce');
+    console.log('Put on mushrooms');
+    console.log('Put on cheese on top');
+    console.log('Put the pizza in the oven');
+    console.log('Bake the pizza at 200 degrees for 10 minutes');
+};
+const makeSushi = function () {
+    console.log('Roll the fish around vegetable');
+    console.log('Put vinegar on the fish');
+    console.log('Press the fish to a suare');
+
+}
+
+//De functie gebruiken (calling a function)
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
